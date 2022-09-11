a007724a4646f04f6333602994d2a6aabab90f33
azozrazoz <85499554+azozrazoz@users.noreply.github.com>
2022-09-11 21:15:58 +0600

    add qr code command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import json
+import qrcode
 from discord.ext import commands
+import os
 
 file = open('config.json', 'r')
 config = json.load(file)
@@ -19,5 +21,20 @@ async def ping(ctx):
 async def ping(ctx: commands.context, *, args):
     result = str(args)
     await ctx.send(embed=discord.Embed(title=f'{result}', description="Hi, this is a test command" , color=0x64ff0a))
+
+@bot.command(name='qr')
+async def make_qr_code(ctx, *, args):
+    file_name = str(args).replace(' ', '_')
+
+    img = qrcode.make(file_name + '.png')
+
+    try:
+        img.save(file_name + '.png')
+    except:
+        print(1)
+
+    await ctx.send(file=discord.File(os.path.abspath(file_name + '.png')))
+
+    os.remove(os.path.abspath(file_name + '.png'))
 
 bot.run(config['token'])
