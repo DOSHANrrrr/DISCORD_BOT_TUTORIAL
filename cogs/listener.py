4ef4feea7325d900de9d4fe132d6ff66efb65513
Дошан Есмаганбет <doshan.de@gmail.com>
2022-10-26 11:13:46 +0600

    user surveillance

---
+++ cogs/listener.py
@@ -0,0 +1,89 @@
+import json
+import discord
+from discord.ext import commands
+import os
+
+
+async def setup(bot):
+    await bot.add_cog(Listener(bot))
+
+
+class Listener(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.data = Listener.get_data_from_json_file()
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f"{member} подключился на {member.guild}")
+        await self.update_member_bot_count_channel_name(member.guild)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        print(f"{member} покинул {member.guild}")
+        await self.update_member_bot_count_channel_name(member.guild)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('BOT ONLINE')
+        if len(self.bot.guilds) > 0:
+            for guild in self.bot.guilds:
+                print(guild)
+                await self.update_member_bot_count_channel_name(guild)
+
+    @staticmethod
+    def get_data_from_json_file():
+        with open(os.path.abspath(os.curdir) + '\\guilds_data.json', 'r', encoding='utf-8') as file:
+            return json.load(file)
+
+    @staticmethod
+    def get_guild_count(guild):
+        count_members = 0
+        count_bots = 0
+
+        for member in guild.members:
+            if not member.bot:
+                count_members += 1
+            else:
+                count_bots += 1
+        return [count_members, count_bots]
+
+    async def update_member_bot_count_channel_name(self, guild):
+        for_members = self.get_guild_member_count_channel_id(guild)
+
+        member_count_channel_id = for_members[0]
+        member_count_suffix = for_members[1]
+
+        for_bots = self.get_guild_bot_count_channel_id(guild)
+
+        bot_count_channel_id = for_bots[0]
+        bot_count_suffix = for_bots[1]
+
+        if member_count_channel_id is not None and member_count_suffix is not None:
+            member_count_channel = discord.utils.get(guild.channels, id=member_count_channel_id)
+            new_name = f"{member_count_suffix}{Listener.get_guild_count(guild)[0]}"
+            print(Listener.get_guild_count(guild)[0])
+            await member_count_channel.edit(name=new_name)
+
+        if bot_count_channel_id is not None and bot_count_suffix is not None:
+            bot_count_channel = discord.utils.get(guild.channels, id=bot_count_channel_id)
+            new_name = f"{bot_count_suffix}{Listener.get_guild_count(guild)[1]}"
+            print(Listener.get_guild_count(guild)[1])
+            await bot_count_channel.edit(name=new_name)
+
+        else:
+            print(f"Не могу обновить количество участников на сервере {guild}, id не был найден")
+
+    def get_guild_member_count_channel_id(self, guild):
+        for data_guild in self.data['guilds']:
+            if int(data_guild['id']) == guild.id:
+                return [data_guild['channel_id_members'], data_guild['suffix_members']]
+
+            return None
+
+    def get_guild_bot_count_channel_id(self, guild):
+        for data_guild in self.data['guilds']:
+            if int(data_guild['id']) == guild.id:
+                return [data_guild['channel_id_bots'], data_guild['suffix_bots']]
+
+            return None
--- main.py
+++ main.py
@@ -1,26 +1,31 @@
+import asyncio
 import discord
 import json
 import qrcode
 from discord.ext import commands
 import os
 
-file = open('config.json', 'r')
-config = json.load(file)
+file_for_config = open('config_copy.json', 'r')
+config = json.load(file_for_config)
 
-bot = commands.Bot(config['prefix'])
 
-@bot.event
-async def on_ready():
-    print('BOT ONLINE')
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(config['prefix'], intents=intents)
+
 
 @bot.command(name='ping')
 async def ping(ctx):
     await ctx.send(f'{ctx.author.mention} pong!')
 
+
 @bot.command(name='foo')
-async def ping(ctx: commands.context, *, args):
+async def foo(ctx: commands.context, *, args):
     result = str(args)
-    await ctx.send(embed=discord.Embed(title=f'{result}', description="Hi, this is a test command" , color=0x64ff0a))
+    await ctx.send(embed=discord.Embed(title=f'{result}', description="Hi, this is a test command", color=0x64ff0a))
+
 
 @bot.command(name='qr')
 async def make_qr_code(ctx, *, args):
@@ -30,11 +35,24 @@ async def make_qr_code(ctx, *, args):
 
     try:
         img.save(file_name + '.png')
-    except:
-        print(1)
+    except Exception as e:
+        print(e)
 
     await ctx.send(file=discord.File(os.path.abspath(file_name + '.png')))
 
     os.remove(os.path.abspath(file_name + '.png'))
 
-bot.run(config['token'])
+
+async def load_cogs():
+    for file in os.listdir('cogs'):
+        if file.endswith('.py'):
+            await bot.load_extension(f"cogs.{file[:-3]}")
+
+
+async def main():
+    await load_cogs()
+    await bot.start(config['token'])
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
