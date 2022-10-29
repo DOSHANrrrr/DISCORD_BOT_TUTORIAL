d0778a3211ee92e38bf71fb4c7c64ca37020c823
Дошан Есмаганбет <doshan.de@gmail.com>
2022-10-29 23:56:13 +0600

    add member online

--- cogs/listener.py
+++ cogs/listener.py
@@ -39,37 +39,48 @@ class Listener(commands.Cog):
     @staticmethod
     def get_guild_count(guild):
         count_members = 0
+        count_members_is_online = 0
         count_bots = 0
 
         for member in guild.members:
+            print(member.status)
+            if member.status is discord.Status.online:
+                count_members_is_online += 1
+
             if not member.bot:
                 count_members += 1
             else:
                 count_bots += 1
-        return [count_members, count_bots]
 
+        return [count_members, count_bots, count_members_is_online]
+
+    @commands.command(name='update')
     async def update_member_bot_count_channel_name(self, guild):
-        for_members = self.get_guild_member_count_channel_id(guild)
+        member_count_channel_id, member_count_suffix = self.get_guild_member_count_channel_id(guild)
 
-        member_count_channel_id = for_members[0]
-        member_count_suffix = for_members[1]
+        bot_count_channel_id, bot_count_suffix = self.get_guild_bot_count_channel_id(guild)
 
-        for_bots = self.get_guild_bot_count_channel_id(guild)
+        member_online_channel_id, member_online_suffix = self.get_guild_members_online_count_channel_id(guild)
 
-        bot_count_channel_id = for_bots[0]
-        bot_count_suffix = for_bots[1]
+        members_count = Listener.get_guild_count(guild)
 
         if member_count_channel_id is not None and member_count_suffix is not None:
             member_count_channel = discord.utils.get(guild.channels, id=member_count_channel_id)
-            new_name = f"{member_count_suffix}{Listener.get_guild_count(guild)[0]}"
-            print(Listener.get_guild_count(guild)[0])
+            new_name = f"{member_count_suffix}{members_count[0]}"
+
             await member_count_channel.edit(name=new_name)
 
-        if bot_count_channel_id is not None and bot_count_suffix is not None:
+        elif bot_count_channel_id is not None and bot_count_suffix is not None:
             bot_count_channel = discord.utils.get(guild.channels, id=bot_count_channel_id)
-            new_name = f"{bot_count_suffix}{Listener.get_guild_count(guild)[1]}"
-            print(Listener.get_guild_count(guild)[1])
+            new_name = f"{bot_count_suffix}{members_count[1]}"
+
             await bot_count_channel.edit(name=new_name)
+
+        elif member_online_channel_id is not None and member_online_suffix is not None:
+            member_online_count_channel = discord.utils.get(guild.channels, id=member_online_channel_id)
+            new_name = f"{member_online_suffix}{members_count[2]}"
+
+            await member_online_count_channel.edit(name=new_name)
 
         else:
             print(f"Не могу обновить количество участников на сервере {guild}, id не был найден")
@@ -85,5 +96,12 @@ class Listener(commands.Cog):
         for data_guild in self.data['guilds']:
             if int(data_guild['id']) == guild.id:
                 return [data_guild['channel_id_bots'], data_guild['suffix_bots']]
+
+            return None
+
+    def get_guild_members_online_count_channel_id(self, guild):
+        for data_guild in self.data['guilds']:
+            if int(data_guild['id']) == guild.id:
+                return [data_guild['channel_id_members_online'], data_guild['suffix_members_online']]
 
             return None
